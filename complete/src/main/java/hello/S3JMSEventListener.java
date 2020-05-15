8472e38dfd334fe29128c79765e97eb4e0352172
Ken Green <ken@kengreen.io>
2020-05-15 14:17:19 -0500

    fixed S3 copy and Dynamo DB insert

--- complete/src/main/java/hello/S3JMSEventListener.java
+++ complete/src/main/java/hello/S3JMSEventListener.java
@@ -1,19 +1,26 @@
 package hello;
 
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Service;
 
-import com.amazonaws.auth.profile.ProfileCredentialsProvider;
 import com.amazonaws.regions.Regions;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.PutItemOutcome;
+import com.amazonaws.services.dynamodbv2.document.Table;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.event.S3EventNotification;
 import com.amazonaws.services.s3.event.S3EventNotification.S3EventNotificationRecord;
-import com.amazonaws.services.s3.model.GetObjectLockConfigurationRequest;
-import com.amazonaws.services.s3.model.S3Object;
-import com.amazonaws.services.s3.model.SetObjectRetentionRequest;
 
 @Service
 public class S3JMSEventListener {
@@ -25,31 +32,92 @@ public class S3JMSEventListener {
 
 		LOG.info("Request: " + request);
 
-		S3EventNotification s3EventNotification = S3EventNotification.parseJson(request);
+		try {
+			
+			S3EventNotification s3EventNotification = S3EventNotification.parseJson(request);
 
-		for (S3EventNotificationRecord s3EventNotificationRecord : s3EventNotification.getRecords()) {
+			for (S3EventNotificationRecord s3EventNotificationRecord : s3EventNotification.getRecords()) {
 
-			AmazonS3 amazonS3 = AmazonS3ClientBuilder.standard()
-					.withCredentials(new ProfileCredentialsProvider())
-					.withRegion(Regions.DEFAULT_REGION).build();
-			
-			String sourceBucketName = s3EventNotificationRecord.getS3().getBucket().getName();
-			String sourceKey = s3EventNotificationRecord.getS3().getObject().getKey();
-			
-			//  TODO: destination bucket name need to include the record stream short name 
-			String destinationBucketName = "poc-archive-target";
-			String targetKey = sourceKey;
-			
-			amazonS3.copyObject(sourceBucketName, sourceKey, destinationBucketName, targetKey);
+				String sourceBucketName = s3EventNotificationRecord.getS3().getBucket().getName();
+				String sourceKey = s3EventNotificationRecord.getS3().getObject().getKey();
 
-			// TODO: compare checksums
-			
-			// TODO: write records to DynamoDB
+				String destinationBucketName = "poc-test-archive";
+				String targetKey = sourceKey;
 
+				try {
+
+					archiveFile(sourceBucketName, sourceKey, destinationBucketName, targetKey);
+
+					updateStatus(destinationBucketName, targetKey, Status.SUCCEEDED);
+					
+				} catch (Exception e) {
+					
+					LOG.error("An exception occurred", e);
+
+					updateStatus(destinationBucketName, targetKey, Status.FAILED);
+				}
+			}
 			
+		} catch (Exception e) { // catch-all 
 			
+			LOG.error("An exception occurred", e);
 		}
+	}
+
+	private void archiveFile(String sourceBucketName, String sourceKey, String destinationBucketName,
+			String targetKey) {
+		
+		LOG.info("Archiving " + sourceBucketName + "/" + sourceKey);
+		
+		AmazonS3 amazonS3 = AmazonS3ClientBuilder.standard()
+				.withRegion(Regions.US_EAST_1).build();
 		
 		
+		//  TODO:
+		// 		- destination bucket name need to include the record stream short name
+		//		- a new folder for each day should be created and the retention policy set on the folder
+		
+		amazonS3.copyObject(sourceBucketName, sourceKey, destinationBucketName, targetKey);
+
+		// TODO: compare checksums
+
+		
+		
+		
+		LOG.info("Archiving " + sourceBucketName + "/" + sourceKey + ": COMPLETE");
+	}
+
+	private void updateStatus(String destinationBucketName, String targetKey, Status status) {
+		
+		LOG.info("Updating status for " + destinationBucketName + "/" + targetKey);
+		
+		AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
+				.enableEndpointDiscovery()
+				.withRegion(Regions.US_EAST_1)
+				.build();
+
+		DynamoDB dynamoDB = new DynamoDB(client);
+		
+		Table table = dynamoDB.getTable("status");
+		
+		Map<String, Object> data = Map.of("s3BucketName", destinationBucketName, "s3Key", targetKey, "status", status.toString());
+		
+		PutItemOutcome putItemOutcome = table.putItem(new Item().withPrimaryKey("date", currentDateYYYYMMDD()).withMap("data", data));
+		
+		LOG.info("putItemOutCome: " + putItemOutcome);
+
+	
+		
+		LOG.info("Updating status for " + destinationBucketName + "/" + targetKey + ": COMPLETE");
+	}
+
+	private static int currentDateYYYYMMDD() {
+
+		return Integer.valueOf(LocalDateTime.now(ZoneOffset.UTC).format(DateTimeFormatter.BASIC_ISO_DATE));
+	}
+	
+	private static enum Status {
+		
+		SUCCEEDED, FAILED
 	}
 }
